d3e2b25735ca500466d4fac499365e4ef5557d66
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2023-10-05 15:29:16 -0700

    Dialog

--- fight-the-heavens/dlg/ohdplanj.d
+++ fight-the-heavens/dlg/ohdplanj.d
@@ -1,7 +1,32 @@
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
-// Update State Trigger for Planetar of Justice                          //
+// Update Dialog for Planetar of Justice                                 //
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 
 REPLACE_TRIGGER_TEXT OHDPLANJ ~Global("OHD_planetar","OH5400",3)~ ~Global("OHD_planetar","OH5400",3) InParty("dorn")~
+
+APPEND OHDPLANJ
+
+  IF ~Global("OHD_planetar","OH5400",3)
+  !InParty("dorn")~ THEN BEGIN s0
+    SAY @100
+    IF ~~ THEN REPLY @101 GOTO stepa
+    IF ~~ THEN REPLY @102 GOTO stepb
+    IF ~~ THEN REPLY @103 GOTO stepc
+  END
+
+  IF ~~ THEN BEGIN stepa
+    SAY @104
+    IF ~~ THEN GOTO stepc
+  END
+
+  IF ~~ THEN BEGIN stepb
+    SAY @105
+    IF ~~ THEN GOTO stepc
+  END
+
+  IF ~~ THEN BEGIN stepc
+    SAY @106
+    IF ~~ THEN DO ~ActionOverride("OHDPLANJ",Enemy()) SetGlobal("OHD_planetar","OH5400",4)~ EXIT
+  END
