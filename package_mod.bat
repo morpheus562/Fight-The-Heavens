d194e941bc108d9ef1686d9901dcbaf60fd46909
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2023-10-05 22:14:30 -0700

    This works?!

--- package_mod.bat
+++ package_mod.bat
@@ -6,7 +6,7 @@ call "%~dp0..\ModPackaging\utilities\ie_games.bat"
 
 REM /* MODIFY: set the values of the 3 variables below to reflect the current mod version */
 set "mod_name=Fight the Heavens"
-set "mod_version=v0.1-alpha"
+set "mod_version=v1.0"
 set mod_folder=fight-the-heavens
 
 REM /* MODIFY: list here which IE games the mod is compatible with, from this list of possibilities: */
